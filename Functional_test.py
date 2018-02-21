9b6aeb7b95c7d93413c9322b1467c7bef9d455a9
Sebastian <snovoa.05@gmail.com>
2018-02-21 17:54:00 -0300

    Function test add check rows

--- Functional_test.py
+++ Functional_test.py
@@ -36,18 +36,32 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as a item in a to-do lists
         inputbox.send_keys(Keys.ENTER)
 
+        import time
+        time.sleep(3)
+
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathes' for row in rows),
-            'New to-do item did not apper in table'
-        )
+
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # There is still a text box inviting her to add another item. she
         # enter "Use peacock feathers to make a fly" (Edith is very methodical)
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
-        # The page updates again, and now both items on her lists
+        time.sleep(3)
+
+        # # The page updates again, and now both items on her lists
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = self.browser.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        
+
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
+        )
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
@@ -57,5 +71,6 @@ class NewVisitorTest(unittest.TestCase):
 
         # Satisfied, dhe goes back to sleep
 
+        self.fail('Finish the test!')
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
