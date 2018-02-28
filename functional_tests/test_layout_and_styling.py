59e5d1f400362d79fcffd441455388dd1289c908
Sebastian <snovoa.05@gmail.com>
2018-02-28 16:32:31 -0300

    Moved FTs into their own individual files

---
+++ functional_tests/test_layout_and_styling.py
@@ -0,0 +1,24 @@
+from .base import FunctionalTest
+
+class LayoutAndStyling(FunctionalTest):
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.server_url)
+        self.browser.set_window_size(1024,768)
+
+        # She notices the input box in nicely center
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] /2,
+            506,
+            delta=5
+        )
+
+        #She starts a new list ans sees rhw input is nicely centered there too
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] /2,
+            506,
+            delta=5
+        )
