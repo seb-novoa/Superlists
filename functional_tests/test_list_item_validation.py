d02531e1827fb09db2407a5342d78e82a40a5fac
Sebastian <snovoa.05@gmail.com>
2018-03-02 22:35:29 -0300

    Implement duplicate item validation at modle layer

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 class ItemValidationTest(FunctionalTest):
+    @skip
     def test_cannot_add_empty_list_items(self):
         # Edith goes to the home page ans accidentally tries to submit an empty list item. She hits ENTER on the empty input box
         self.browser.get(self.server_url)
@@ -45,3 +46,17 @@ class ItemValidationTest(FunctionalTest):
 
         self.check_for_row_in_list_table('1: Buy milk')
         self.check_for_row_in_list_table('2: Make tea')
+
+    def test_cannot_add_duplicate_items(self):
+        #Edith goes to the home page and stars a new list
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('Buy wellies', Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy wellies')
+
+        # She accidentally tries to enter a duplicate item
+        self.get_item_input_box().send_keys('Buy wellies', Keys.ENTER)
+
+        # She sees a helpful error message
+        self.check_for_row_in_list_table('1: Buy wellies')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You´ve already got this in your list")
--- lists/forms.py
+++ lists/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from lists.models import Item
 
 EMPTY_LIST_ERROR = "You can´t have an empty list item"
+
 class ItemForm(forms.models.ModelForm):
     class Meta:
         model = Item
@@ -15,3 +16,7 @@ class ItemForm(forms.models.ModelForm):
         error_messages = {
             'text': {'required': EMPTY_LIST_ERROR }
         }
+
+    def save(self, for_list):
+        self.instance.list = for_list
+        return super().save()
